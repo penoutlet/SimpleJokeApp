6e1cfa04eb287884af8fc8470dd5559269144233
Sam <samuelriley1393@gmail.com>
2019-12-27 22:02:39 -0500

    adding to git

---
+++ src/main/java/com/sam/chucknorris/ChucknorrisApplication.java
@@ -0,0 +1,13 @@
+package com.sam.chucknorris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChucknorrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChucknorrisApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sam/chucknorris/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.sam.chucknorris.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
